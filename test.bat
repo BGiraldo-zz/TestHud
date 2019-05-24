6ff5e70e4508d36e510de2e0e913b4e56554f1d9
Brayan Giraldo <brayan.giraldo@yuxiglobal.com>
2019-05-24 17:13:34 -0500

    test

--- test.bat
+++ test.bat
@@ -1,9 +1,9 @@
 git checkout --track -b Dev origin/Dev
-if %errorlevel% neq 128 exit /b %errorlevel%
+if %errorlevel% neq 128 if %errorlevel% neq 0 exit /b %errorlevel%
 git checkout Dev
 if %errorlevel% neq 0 exit /b %errorlevel%
 git checkout --track -b QA origin/QA
-if %errorlevel% neq 128 exit /b %errorlevel%
+if %errorlevel% neq 128 if %errorlevel% neq 0 exit /b %errorlevel%
 git checkout QA 
 if %errorlevel% neq 0 exit /b %errorlevel%
 git merge Dev
